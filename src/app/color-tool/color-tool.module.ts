d42b8580f65ffd9f8506520c13026472c047ba4a
Harold Baguinon <HBaguinon@gmail.com>
2020-02-24 13:39:05 -0500

    added tool-header component shared module

--- src/app/color-tool/color-tool.module.ts
+++ src/app/color-tool/color-tool.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
 @NgModule({
   declarations: [ColorHomeComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   exports: [ ColorHomeComponent ],
 })
