5998f7dca450aeaf8d04f0fd20865a0e0d0de4b7
Harold Baguinon <HBaguinon@gmail.com>
2020-02-24 09:55:17 -0500

    added JavaScript link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'demo-app';
+  title = "demo-app";
+
+  //
+  // In JavaScript, it looks like this:
+  //
+  // title: string;
+  //
+  // constructor() {
+  //   this.title = 'demo-app';
+  // }
 }
+// Mastering JavaScript is KEY
+// A good resource is "You Don't Know JS"
+// https://github.com/getify/You-Dont-Know-JS
+// https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/get-started/ch1.md
