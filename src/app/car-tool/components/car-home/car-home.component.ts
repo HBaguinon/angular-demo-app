dd018764cd6fbd90e37845c7ff42d2f7a137a516
Harold Baguinon <HBaguinon@gmail.com>
2020-02-24 13:15:52 -0500

    cleaned up car component

--- src/app/car-tool/components/car-home/car-home.component.ts
+++ src/app/car-tool/components/car-home/car-home.component.ts
@@ -17,7 +17,6 @@ export class CarHomeComponent implements OnInit {
     {id: 2, make: 'Toyota', model: '86', year: 2017, color: 'red', price: 26000 }
   ];
 
-  // newCar: Car;
   // can do this:
   // cars = [
   //   { id: 1, make: 'Toyota', model: 'Celica GT-Four', year: 1997, color: 'black', price: 6000 }
